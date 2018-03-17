c1bd9222fb303a8b2d1f56385d26fc5e33f3de56
Matthieu Court <matthieu.court@gmail.com>
2018-03-16 22:48:26 -0400

    Now returning the formatted table with the setup for the Linear Program

--- server.R
+++ server.R
@@ -3,22 +3,25 @@ library(leaflet)
 library(ggmap)
 library(leaflet.extras)
 library(magrittr)
+library(lpSolve)
+library(plyr)
+library(geosphere)
+library(shinyjs)
 
-shinyServer(
-  function(input, output, session){
-    
-    #data loading jobs
-    pedestrianTraffic <<- read.csv("city_of_Toronto_pedestrian_traffic_open_data.csv", TRUE, ",")
-    class(pedestrianTraffic)
-    
-    yogaGyms <<- read.csv("yoga_studio_locations_more.csv", TRUE, ",")
-    class(yogaGyms)
-    
-  # runs after the do button (get map button) is clicked  
+shinyServer(function(input, output, session) {
+  #data loading jobs
+  observeEvent(input$refresh,{
+    session$reload()
+  })
+  pedestrianTraffic <<- read.csv("city_of_Toronto_pedestrian_traffic_open_data.csv", TRUE, ",")
+  class(pedestrianTraffic)
+  
+  yogaGyms <<- read.csv("yoga_studio_locations_more.csv", TRUE, ",")
+  class(yogaGyms)
+  
+  # runs after the do button (get map button) is clicked
   observeEvent(input$do, {
-    
     output$mymap <- renderLeaflet({
-      
       getLocation <- input$address
       userLocation <- geocode(getLocation)
       
@@ -26,7 +29,57 @@ shinyServer(
       locationLon <<- userLocation$lon
       locationLat <<- userLocation$lat
       
-      icon.glyphicon <- makeAwesomeIcon(icon= 'flag', markerColor = 'blue', iconColor = 'black')
+      
+      #getMUserToHotspots <- function(i) {
+      #  mapdist(as.numeric(pedestrianTraffic[i, c ('Longitude', 'Latitude')]),
+      #         as.numeric(locationLon, locationLat))$m
+      #}
+      
+      
+      newPedestrianTraffic <- head(arrange(pedestrianTraffic, desc(pedestrianVolume)), n = 10)
+      class(newPedestrianTraffic)
+      newPedestrianTraffic$distanceToUser <- NA
+      
+      # print(newPedestrianTraffic)
+      
+      
+      #from <- c(newPedestrianTraffic[5, "Longitude"], newPedestrianTraffic[5, "Latitude"])
+      #to <- c(locationLon, locationLat)
+      #test <- mapdist(from, to)
+      #test2 <- (test$m)
+      #print(test2)
+      #print(typeof(test2))
+      #newPedestrianTraffic[2, "distanceToUser"] = test2
+      #print(newPedestrianTraffic)
+      
+      
+      for (i in 1:nrow(newPedestrianTraffic)) {
+        from <- c(newPedestrianTraffic[i, "Longitude"], newPedestrianTraffic[i, "Latitude"])
+        to <- c(locationLon, locationLat)
+        test <- distm(from, to, fun = distHaversine)
+        test2 <- (test)
+        
+        newPedestrianTraffic[i, "distanceToUser"] = test
+      }
+      
+      newPedestrianTraffic$dToUserXWeight <- newPedestrianTraffic$pedestrianVolume * newPedestrianTraffic$distanceToUser
+      toMinimize <- sum(newPedestrianTraffic$dToUserXWeight)
+      print(newPedestrianTraffic)
+      cat("with the user long lat at first the value to minimize is", toMinimize)
+      
+      # computational duplicate vars for the solver
+      # x <- locationLat
+      # y <- locationLon
+      
+      # print(newPedestrianTraffic)
+      # Write piece of function that optimizes user latlong such that he is closest to population hubs
+      # f <- function(a,b) sum(newPedestrianTraffic[i, "pedestrianVolume"] * distm(c(newPedestrianTraffic[i, "Longitude"], newPedestrianTraffic[i, "Latitude"]), c(a, b)))
+      # ymin <- optimize(f, c(x,y), tol = 0.0001, b=locationLat)
+      # print(ymin)
+      
+      icon.glyphicon <- makeAwesomeIcon(icon = 'flag',
+                        markerColor = 'blue',
+                        iconColor = 'black')
       userLocationRow <- data.frame(Name = 'Your Location', x = locationLon, y = locationLat)
       yogaGyms <- rbind(yogaGyms, userLocationRow)
       
@@ -34,11 +87,11 @@ shinyServer(
       
       getColor <- function(yogaGyms) {
         sapply(yogaGyms$Name, function(Name) {
-          if(Name == "Your Location") {
+          if (Name == "Your Location") {
             "green"
           } else {
             "beige"
-          }  
+          }
         })
       }
       
@@ -49,7 +102,7 @@ shinyServer(
           if (pedestrianVolume <= 7500) {
             "red"
           }
-          else if(pedestrianVolume <= 15000) {
+          else if (pedestrianVolume <= 15000) {
             "red"
           }
           else {
@@ -69,36 +122,32 @@ shinyServer(
       
       leaflet(data = yogaGyms) %>%
         addTiles() %>%
-        setView(lng = locationLon, lat = locationLat, zoom=14) %>%
-        addAwesomeMarkers(lng = ~x, lat = ~y, icon = icons, label = ~as.character(Name))
+        setView(lng = locationLon, lat = locationLat, zoom = 14) %>%
+        addAwesomeMarkers(lng = ~ x, lat = ~ y, icon = icons, label = ~ as.character(Name))
       
-    # end of the render leaflet
+      # end of the render leaflet
     })
     
-      leafletProxy("mymap", session = shiny::getDefaultReactiveDomain(), 
-                   data = pedestrianTraffic,
-                   deferUntilFlush = TRUE) %>% 
-        
-                   addTiles() %>%
-                   addCircleMarkers(radius = ~pedestrianVolume/1000, 
-                                    lng = ~Longitude, 
-                                    lat = ~Latitude, 
-                                    weight = 5, color = "red", stroke = FALSE, fillOpacity = 0.3)
-      
-  # end of the on get map button click    
+    leafletProxy("mymap",session = shiny::getDefaultReactiveDomain(),data = pedestrianTraffic,deferUntilFlush = TRUE) %>%
+      addTiles() %>%
+      addCircleMarkers(radius = ~ pedestrianVolume / 1000,lng = ~ Longitude,lat = ~ Latitude, weight = 5, color = "red", stroke = FALSE, fillOpacity = 0.3)
+    
+    # end of the on get map button click
   })
   
-    output$testPlot <- renderPlot({
-      distType <- input$Distribution
-      size <- input$sampleSize
-      
-      if(distType == "Normal"){
-        randomVec <- rnorm(size, mean = as.numeric(input$mean), sd=as.numeric(input$sd))
-      }
-      else {
-        randomVec <- rexp(size, rate = 1/as.numeric(input$lambda))
-      }
-      hist(randomVec, col="orange")
-    })
-  }
-)
+  output$testPlot <- renderPlot({
+    distType <- input$Distribution
+    size <- input$sampleSize
+    
+    if (distType == "Normal") {
+      randomVec <-
+        rnorm(size,
+              mean = as.numeric(input$mean),
+              sd = as.numeric(input$sd))
+    }
+    else {
+      randomVec <- rexp(size, rate = 1 / as.numeric(input$lambda))
+    }
+    hist(randomVec, col = "orange")
+  })
+})
--- ui.R
+++ ui.R
@@ -3,45 +3,44 @@ library(utils)
 library(ggmap)
 library(ggplot2)
 library(leaflet)
+library(lpSolve)
+library(plyr)
+library(geosphere)
+library(shinyjs)
 
 #Change the wd to where you have the folder with your UI and Server files
 setwd("C:/Users/matth/Desktop/shiny_tests")
 
-shinyUI(
-    pageWithSidebar(
-    headerPanel(h2("protein, water, liquor"), windowTitle = "congenial palm tree"),
+shinyUI(pageWithSidebar(
+  headerPanel("protein, water, liquor", windowTitle = "congenial palm tree"),
+  
+  sidebarPanel(
+    # Not using these dd s for the moment nigs
     
-    sidebarPanel(
-      
-      # Not using these dd s for the moment nigs
-      
-      #selectInput("Distribution", "Please select Distribution Type",
-      #            choices=c("Normal", "Exponential")),
-      #selectInput("endTime", "Please select the time you end work",
-      #            choices=c("3 PM", "4 PM", "5 PM", "6 PM", "7 PM")),
-      
-      # This one is cooking 
-      h3("Persistence pays off"),
-      img(src = "sakufun.jpg", width = "100%"),
-      sliderInput("workDay", h4("Your work hours..."),
-                   min=0, max=23, value=c(9,20), step=0.5, post= " hrs", sep = ",", animate = TRUE),
-      
-      # Boiler plate tests with basic R functions
-      #conditionalPanel(condition = "input.Distribution == 'Normal' ",
-      #                 textInput("mean", "Please select the mean", 10),
-      #                 textInput( "sd", "Please select the standard deviation", 3)),
-      #conditionalPanel(condition = "input.Distribution == 'Exponential' ",
-      #                 textInput("lambda", "Please Select Eponential Lambda: ", 1)),
-      
-      # textbox that grabs the current user location
-      
-      textInput("address", h4("Your address (GTA only)"),
-                      value = "Toronto"),
-      actionButton("do", "get map", icon("map"), style = "color: #ee7600; background-color: #deeff5")
-    ),
-    mainPanel(
-      #plotOutput("testPlot"),
-      leafletOutput("mymap")
-    )
-  )
-)
+    #selectInput("Distribution", "Please select Distribution Type",
+    #            choices=c("Normal", "Exponential")),
+    #selectInput("endTime", "Please select the time you end work",
+    #            choices=c("3 PM", "4 PM", "5 PM", "6 PM", "7 PM")),
+    
+    # This one is cooking
+    h3("Persistence pays off"),
+    #img(src = "sakufun.jpg", width = "100%"),
+    sliderInput("workDay", h4("Your work hours..."), min = 0, max = 23, value = c(9, 20), step = 0.5, post = " hrs", sep = ",", animate = TRUE),
+    
+    # Boiler plate tests with basic R functions
+    #conditionalPanel(condition = "input.Distribution == 'Normal' ",
+    #                 textInput("mean", "Please select the mean", 10),
+    #                 textInput( "sd", "Please select the standard deviation", 3)),
+    #conditionalPanel(condition = "input.Distribution == 'Exponential' ",
+    #                 textInput("lambda", "Please Select Eponential Lambda: ", 1)),
+    
+    # textbox that grabs the current user location
+    
+    textInput("address", h4("Your address (GTA only)"),
+              value = "Yonge and Dundas"),
+    actionButton("do", "get map", icon("map")),#, style = "color: #ee7600"),
+    actionButton("refresh", "refresh", style = "color: #ee7600")
+  ),
+  mainPanel(#plotOutput("testPlot"),
+    leafletOutput("mymap"))
+))
