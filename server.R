f003b834814f087f0f1adf1dc4b8028b80c37c44
Matthieu Court <matthieu.court@gmail.com>
2018-03-23 06:28:07 -0400

    everything working except weighted hot spot vectors and need to polish it up

--- server.R
+++ server.R
@@ -12,6 +12,8 @@ library(RCurl)
 library(XLConnect)
 library(googleway)
 library(jsonlite)
+library(rsconnect)
+#rsconnect::deployApp('C:/Users/matth/Desktop/shiny_tests')
 
 shinyServer(function(input, output, session) {
   #data loading jobs
@@ -30,37 +32,52 @@ shinyServer(function(input, output, session) {
   webImport = webImport[-c(3,5,6,10)]
   print(webImport)
   
-  key <- 'AIzaSyChVlEEFBjIaVbni1rakTQiPzGhdpcWtOc'
-  googlePull <- google_places(radar = TRUE, location = c(43.810242, -79.398503), radius = 5000, place_type = "gym", key = key)
-  yogaLon = googlePull$results$geometry$location$lng
-  yogaLat = googlePull$results$geometry$location$lat
   
-  yogaGyms <<- read.csv("yoga_studio_locations_more.csv", TRUE, ",")
-  class(yogaGyms)
+  #yogaGyms <<- read.csv("yoga_studio_locations_more.csv", TRUE, ",")
+  #class(yogaGyms)
+  observeEvent(input$load, {
   
-  #More Google Places API tests
-  
-  #key <- 'AIzaSyB_d8paMHo6VkQ0XfwW9UznmOPyXdlJ_SI'
-  #res <- google_places(location = c(-79.39679, 43.72624),
-  #                     place_type = "yoga",
-  #                     radius = 20000,
-  #                     key = key)
-  #print(typeof(res))
-  #res2 <- fromJSON(res, simplifyVector = TRUE,
-  #                 simplifyDataFrame = simplifyVector,
-  #                 simplifyMatrix = simplifyVector)
-  #print(res[3])
-  
+  })
   # runs after the do button (get map button) is clicked
   observeEvent(input$do, {
+    
     output$mymap <- renderLeaflet({
       withProgress(message = 'loading map...', value = 1, {
-      getLocation <- input$address
-      userLocation <- geocode(getLocation)
       
-      # Globalized instead of a subclass implementation to use on multiple layers of the map
-      locationLon <<- userLocation$lon
-      locationLat <<- userLocation$lat
+      #type of location the user wants to search for 
+        getLocation <<- input$address
+        userLocation <<- geocode(getLocation)
+        
+        # Globalized instead of a subclass implementation to use on multiple layers of the map
+        locationLon <<- userLocation$lon
+        locationLat <<- userLocation$lat
+        
+        locationType <- input$locationType
+        key <- 'AIzaSyChVlEEFBjIaVbni1rakTQiPzGhdpcWtOc'
+        googlePull <<- google_places(radar = TRUE, location = c(locationLat, locationLon), radius = 500, keyword = locationType, key = key)
+        yogaLon = googlePull$results$geometry$location$lng
+        yogaLat = googlePull$results$geometry$location$lat
+        
+        cleanGooglePull <- googlePull$results
+        cleanGooglePull = cleanGooglePull[-c(3,4)]
+        cleanGooglePull = cleanGooglePull[c(2,1)]
+        
+        class(cleanGooglePull)
+        colnames(cleanGooglePull)[colnames(cleanGooglePull) == "geometry.location.lng"] <- "x"
+        colnames(cleanGooglePull)[colnames(cleanGooglePull) == "geometry.location.lat"] <- "y"
+        colnames(cleanGooglePull)[colnames(cleanGooglePull) == "id"] <- "Name"
+        
+        cleanGooglePull$Name <- "Rival"
+        
+        Name <- cleanGooglePull$Name
+        x <- googlePull$results$geometry$location$lng
+        y <- googlePull$results$geometry$location$lat
+        
+        yogaGyms <<- data.frame(Name,
+                                x,
+                                y)
+        class(yogaGyms)
+        print(yogaGyms)
       
       incProgress(1, message = "Cleaning up some data...")
       
@@ -85,49 +102,97 @@ shinyServer(function(input, output, session) {
       class(locations)
       locations2 <- locations[,c(2,1)]
       class(locations2)
-      print(locations2)
+      #print(locations2)
       
       incProgress(3, message = "running our top secret algorithm...")
       constraint <- input$distConstraint
       
-      #Minimization optimization
-      min.RSS <- function(data, par) {
-        with(data, sum(distm(locations2, par)))
-      }
+      optimType <- input$optimType
       
-      (result <- optim(par = c(locationLon, locationLat), 
-                       min.RSS,
-                       lower = c(locationLon - constraint*0.009, locationLat - constraint*0.009),
-                       upper = c(locationLon + constraint*0.009, locationLat + constraint*0.009),
-                       data = locations2,
-                       method = "BFGS"))
+      userLocationRow <- data.frame(Name = 'Your Original Location', x = locationLon, y = locationLat)
+     # yogaGyms <- rbind(yogaGyms, userLocationRow)
       
       yogaLocations = data.frame(x=yogaGyms$x,
                                  y=yogaGyms$y)
       
-      #Maximization optimization
-      
-      max.RSS <- function(data, par) {
-        with(data, sum(distm(yogaLocations, par)))
+      if(optimType == "Minimize Dist. from Popular Spots"){
+        
+        min.RSS <- function(data, par) {
+          with(data, sum(distm(locations2, par)))
+        }
+        
+        (result <- optim(par = c(locationLon, locationLat), 
+                         min.RSS,
+                         lower = c(locationLon - constraint*0.009, locationLat - constraint*0.009),
+                         upper = c(locationLon + constraint*0.009, locationLat + constraint*0.009),
+                         data = locations2,
+                         method = "BFGS"))
+        
+        minimizedLon <- result$par[1]
+        minimizedLat <- result$par[2]
+        
+        minimizedLocationRow <- data.frame(Name = 'Minimized Location', x = minimizedLon, y = minimizedLat)
+        yogaGyms <- rbind(yogaGyms, minimizedLocationRow)
+        
+        
+      }else if(optimType == "Maximize Dist. from Competitors"){
+        
+        max.RSS <- function(data, par) {
+          with(data, sum(distm(yogaLocations, par)))
+        }
+        
+        (result2 <- optim(par = c(locationLon, locationLat), 
+                          max.RSS,
+                          lower = c(locationLon - constraint*0.3*0.009, locationLat - constraint*0.3*0.009),
+                          upper = c(locationLon + constraint*0.3*0.009, locationLat + constraint*0.3*0.009),
+                          control = list(fnscale = -1),
+                          data = yogaLocations,
+                          method = "BFGS"))
+        
+        
+        maximizedLon <- result2$par[1]
+        maximizedLat <- result2$par[2]
+        
+        maximizedLocationRow <- data.frame(Name = 'Maximized Location', x = maximizedLon, y = maximizedLat)
+        yogaGyms <- rbind(yogaGyms, maximizedLocationRow)
+        
+      }else{
+        min.RSS <- function(data, par) {
+          with(data, sum(distm(locations2, par)))
+        }
+        
+        (result3 <- optim(par = c(locationLon, locationLat), 
+                          min.RSS,
+                          lower = c(locationLon - constraint*0.009, locationLat - constraint*0.009),
+                          upper = c(locationLon + constraint*0.009, locationLat + constraint*0.009),
+                          data = locations2,
+                          method = "BFGS"))
+        
+        optimized0Lon <- result3$par[1]
+        optimized0Lat <- result3$par[2]
+        
+        max.RSS <- function(data, par) {
+          with(data, sum(distm(yogaLocations, par)))
+        }
+        
+        (result4 <- optim(par = c(optimized0Lon, optimized0Lat), 
+                          max.RSS,
+                          lower = c(optimized0Lon - constraint*0.3*0.009, optimized0Lat - constraint*0.3*0.009),
+                          upper = c(optimized0Lon + constraint*0.3*0.009, optimized0Lat + constraint*0.3*0.009),
+                          control = list(fnscale = -1),
+                          data = yogaLocations,
+                          method = "BFGS"))
+        
+        optimizedLon <- result4$par[1]
+        optimizedLat <- result4$par[2]
+        
+        optimizedLocationRow <- data.frame(Name = 'Combo-Optimized Location', x = optimizedLon, y = optimizedLat)
+        yogaGyms <- rbind(yogaGyms, optimizedLocationRow)
+        
       }
       
-      (result2 <- optim(par = c(locationLon, locationLat), 
-                        max.RSS,
-                        lower = c(locationLon - constraint*0.3*0.009, locationLat - constraint*0.3*0.009),
-                        upper = c(locationLon + constraint*0.3*0.009, locationLat + constraint*0.3*0.009),
-                        control = list(fnscale = -1),
-                        data = yogaLocations,
-                        method = "BFGS"))
-      
-      print(result2$par[1])
-      print(result2$par[2])
-      
-      maximizedLon <- result2$par[1]
-      maximizedLat <- result2$par[2]
-      
-      maximizedLocation <- data.frame(Name = 'Maximized Location', x = maximizedLon, y = maximizedLat)
-      
-      
+      # adding the updated user location to the map
+      yogaGyms <- rbind(yogaGyms, userLocationRow)
       
       newPedestrianTraffic$dToUserXWeight <- newPedestrianTraffic$pedestrianVolume * newPedestrianTraffic$distanceToUser
       toMinimize <- sum(newPedestrianTraffic$dToUserXWeight)
@@ -135,41 +200,47 @@ shinyServer(function(input, output, session) {
       cat("with the user long lat at first the value to minimize is", toMinimize)
       incProgress(4, message = "Finishing up...")
       cat("")
-      print(result$par[1])
-      print(result$par[2])
+      #print(result$par[1])
+      #print(result$par[2])
       
-      optimalLon <- result$par[1]
-      optimalLat <- result$par[2]
+      #optimalLon <- result$par[1]
+      #optimalLat <- result$par[2]
       
       icon.glyphicon <- makeAwesomeIcon(icon = 'flag',
                         markerColor = 'blue',
                         iconColor = 'black')
-      userLocationRow <- data.frame(Name = 'Your Location', x = locationLon, y = locationLat)
-      bestLocationRow <- data.frame(Name = 'Best Location', x = optimalLon, y = optimalLat)
+     
       
-      yogaGyms <- rbind(yogaGyms, userLocationRow)
-      yogaGyms <- rbind(yogaGyms, bestLocationRow)
-      yogaGyms <- rbind(yogaGyms, maximizedLocation)
+      #userLocationRow <- data.frame(Name = 'Your Location', x = locationLon, y = locationLat)
+      #bestLocationRow <- data.frame(Name = 'Best Location', x = optimalLon, y = optimalLat)
+      
+      #yogaGyms <- rbind(yogaGyms, userLocationRow)
+      #yogaGyms <- rbind(yogaGyms, bestLocationRow)
+      #yogaGyms <- rbind(yogaGyms, maximizedLocation)
       #print(yogaGyms)
       
-      print(typeof(optimalLon))
-      print(typeof(locationLon))
+      #print(typeof(optimalLon))
+      #print(typeof(locationLon))
       
       # sets the color for the yoga gym markers
       
       getColor <- function(yogaGyms) {
         sapply(yogaGyms$Name, function(Name) {
-          if (Name == "Your Location") {
+          if (Name == "Your Original Location") {
             "green"
-          }
-          else if (Name == "Best Location") {
+            
+          } else if (Name == "Minimized Location") {
             "blue"
-          }
-          else if(Name == "Maximized Location") {
+            
+          } else if(Name == "Maximized Location") {
             "red"
-          }
-          else {
+            
+          } else if (Name == "Combo-Optimized Location"){
+            "white"
+            
+          } else {
             "beige"
+            
           }
         })
       }
@@ -205,7 +276,7 @@ shinyServer(function(input, output, session) {
         addAwesomeMarkers(lng = ~ x, lat = ~ y, icon = icons, label = ~ as.character(Name))
       
       
-      print(googlePull)
+      #print(googlePull)
       })
       # end of the render leaflet
     })
--- ui.R
+++ ui.R
@@ -41,6 +41,9 @@ shinyUI(pageWithSidebar(
     textInput("address", h5("where in Toronto do you want to start your business"),
               value = "Yonge and Dundas"),
     sliderInput("distConstraint", h5("Optimize within this distance (in km).."), min = 0, max = 5, value = 3, step = 0.25, post = " km", animate = TRUE),
+    #actionButton("load", "load data", style = "color: #6666ff"),
+    selectInput("optimType", h5("How do you want our system to determine the optimal location?"), choices = c("Minimize Dist. from Popular Spots", "Maximize Dist. from Competitors", "Minimization/Maximization Combination")),
+    
     actionButton("do", "get map", icon("map"), style = "color: #6666ff"),
     actionButton("refresh", "refresh", style = "color: #6666ff")
   ),
